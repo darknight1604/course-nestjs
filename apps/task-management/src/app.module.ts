f1fec19d4a525d656bfe79be112c0be0ee8173de
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-08-21 22:01:31 +0700

    feat: add LoggerMiddleware to log requests in the application

--- apps/task-management/src/app.module.ts
+++ apps/task-management/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@task-management/modules/authentication/auth.module';
 import { UsersModule } from '@task-management/modules/users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from '@task-management/middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -29,4 +30,8 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*'); // Apply LoggerMiddleware to all routes
+  }
+}
