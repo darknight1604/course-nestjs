812614d23f098cfbf147643c90026d05e7b10a8e
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-09-14 19:53:01 +0700

    feat: use getAllAndMerge at RolesGuard

--- apps/task-management/src/modules/authentication/guards/roles.guard.ts
+++ apps/task-management/src/modules/authentication/guards/roles.guard.ts
@@ -12,7 +12,10 @@ export class RolesGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const roles = this.reflector.get(Roles, context.getHandler());
+    const roles = this.reflector.getAllAndMerge(Roles, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     if (!roles) {
       return true;
     }
