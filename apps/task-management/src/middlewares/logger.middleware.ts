6bee4d143d830e1f7f253cb3546a3b137f351616
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-09-14 16:00:14 +0700

    refactor: remove console.log

--- apps/task-management/src/middlewares/logger.middleware.ts
+++ apps/task-management/src/middlewares/logger.middleware.ts
@@ -1,12 +1,13 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(LoggerMiddleware.name);
   use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl } = req;
 
-    console.log(`[${new Date().toISOString()}] ${method} ${originalUrl}`);
+    this.logger.log(`[${new Date().toISOString()}] ${method} ${originalUrl}`);
     next();
   }
 }
