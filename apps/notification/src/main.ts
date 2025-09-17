f2456eb0e9f79f13002ece56da566289ef98b570
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-09-17 21:39:39 +0700

    feat: rename notification.service to app.service

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { NotificationModule } from './notification.module';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import 'reflect-metadata';
+
+declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.NOTIFICATION_APP_PORT ?? 3000);
+
+  if (module && module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
+
 void bootstrap();
