26e87c81a28005b5fd9bb4fbbc2cd9b959e6f8f3
Huỳnh Quốc Tấn <65750436+darknight1604@users.noreply.github.com>
2025-09-21 22:10:27 +0700

    refactor: remove required createdById ticket

--- apps/task-management/src/modules/tickets/dto/create-ticket.dto.ts
+++ apps/task-management/src/modules/tickets/dto/create-ticket.dto.ts
@@ -1,14 +1,11 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTicketDto {
   @IsString()
   @IsNotEmpty()
   title: string;
   description?: string;
-
-  @IsNumber()
-  @IsNotEmpty()
-  createdById: number;
+  createdById?: number;
   createdBy?: string;
   assigneeId?: number;
   status?: string;
